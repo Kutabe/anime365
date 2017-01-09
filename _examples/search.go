4f4e4dd1a96e872666e7e06a6aafc1eb3985dbd5
Kutabe <kutabe@ex.ua>
2017-01-09 05:48:36 +0200

    Fixed more typos and created _examples directory

---
+++ _examples/search.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/kutabe/anime365"
+)
+
+func main() {
+	parameters := make(map[string]string)
+	parameters["query"] = "Bakemonogatari"
+	respSeries, err := anime365.GetSeries(parameters)
+	if err != nil {
+		log.Panic(err)
+	}
+	for _, series := range respSeries {
+		fmt.Printf("Title: %s\nDescription:\n", series.Title)
+		for _, description := range series.Descriptions {
+			fmt.Printf("%s:\n\t%s\n", description.Source, description.Value)
+		}
+		fmt.Print("Episodes:\n")
+		for _, episode := range series.Episodes {
+			fmt.Printf("#%s\n", episode.EpisodeInt)
+			fmt.Printf("Translations:\n")
+			respEpisode, err := anime365.GetEpisodeByID(int(episode.ID), nil)
+			if err != nil {
+				log.Println(err)
+			}
+			for _, translation := range respEpisode.Translations {
+				fmt.Printf("\tLang: %s \n\tAuthors: %s \n\tDownload link: http://smotret-anime.ru/translations/mp4/%d\n", translation.TypeLang, translation.AuthorsSummary, translation.ID)
+			}
+
+		}
+	}
+}
